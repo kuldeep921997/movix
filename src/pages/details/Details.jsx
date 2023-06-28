d222382102693e7efc4ac2f559bd67715e45077e
kuldeep.lodhi <kuldeep.lodhi@algonauts.in>
2023-06-28 14:05:19 +0530

    there is an error which fails the containt type in url - V1

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -14,17 +14,19 @@ const Details = () => {
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`)
   const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
 
-  return (
-
-    <div>
-      <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
-      <Cast data={credits?.cast} loading={creditsLoading} />
-      <VideosSection data={data} loading={loading} />
-      <Similar mediaType={mediaType} id={id} />
-      <Recommendation mediaType={mediaType} id={id} />
-    </div>
-
-  )
+  {
+    if (data) {
+      return (
+        <div>
+          <DetailsBanner video={data?.results? data.results[0] : ''} crew={credits?.crew} />
+          <Cast data={credits?.cast} loading={creditsLoading} />
+          <VideosSection data={data} loading={loading} />
+          <Similar mediaType={mediaType} id={id} />
+          <Recommendation mediaType={mediaType} id={id} />
+        </div>
+      )
+    }
+  }
 }
 
 export default Details
